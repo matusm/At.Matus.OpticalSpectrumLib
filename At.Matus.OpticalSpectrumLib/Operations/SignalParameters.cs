9fb19b693fbf33fad329ae67a46650e31d145141
Michael Matus <michael@matus.at>
2025-12-02 13:30:53 +0100

    Refactor signal statistics

---
+++ At.Matus.OpticalSpectrumLib/Operations/SignalParameters.cs
@@ -0,0 +1,23 @@
+﻿namespace At.Matus.OpticalSpectrumLib
+{
+    public static class SignalParameters
+    {
+        public static StatisticPod.StatisticPod GetSignalStatistics(this IOpticalSpectrum spectrum)
+        {
+            var stats = new StatisticPod.StatisticPod();
+            foreach (var dp in spectrum.DataPoints)
+                stats.Update(dp.Signal);
+            return stats;
+        }
+
+        public static double GetMaximumSignal(this IOpticalSpectrum spectrum)
+        {
+           return spectrum.GetSignalStatistics().MaximumValue;
+        }
+
+        public static double GetMinimumSignal(this IOpticalSpectrum spectrum)
+        {
+            return spectrum.GetSignalStatistics().MinimumValue;
+        }
+    }
+}
